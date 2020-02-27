a1e59632ba8c0bfbdee9e3a28de1627c300fdb93
zawis <piere01@op.pl>
2020-02-27 14:31:27 +0100

    initial commit

---
+++ start.py
@@ -0,0 +1,75 @@
+# print("hello world!")
+
+# tekst = "hello world!"
+# tekst = 'hello world!'
+# tekst = """hello 
+# world!"""
+# tekst = 0
+# print(tekst)
+# print(type(tekst))
+# print(type("ala"))
+# print(type(5))
+# print(type(5.5))
+# print(type(True))
+# print(type(None))
+# print(5+8)
+# print(8-5)
+# print(5*5)
+# print(5/8)
+# print(7//14)#bez reszty
+# print(5%5)#modulo
+# print(5**5)#potęga
+# print("ala " + "ma kota")
+# print("ala ma " + str(5) + " lat")
+# liczba = int("100")
+# print("ala " * 10)
+
+
+#LISTY
+lista = []
+print(type(lista))
+lista2 = [1,2,3]
+print(lista2[0])
+imie="magdalena"
+print(imie[0])
+lista2[0] = 5 #to zadziała
+#imie[0] = "K" #to chuja da
+
+print(imie.swapcase()) #tylko wyświetla
+
+imie = imie.swapcase() #zmienia
+print(imie)
+
+"ala".swapcase()
+lista3 = [1,"ala",5.5,None,True,[1,2]]
+lista3[5][1]
+macierz = [
+[1,2,3],
+[4,5,6],
+[7,8,9]
+]
+macierz[1][1] #5
+nowa = lista2 + macierz
+
+
+#SŁOWNIK
+slownik = {}
+slownik['imie'] = 'adam'
+slownik[0] = 'adam'
+print(slownik)
+slownik2 = {'imie': 'adam', 0: 'adam'}
+print(slownik2.keys())
+print(slownik2.values())
+
+def pow():
+    pass
+    slownik2 = {'imie': 'adam', 0: 'adam'}
+    print(slownik2.keys())
+    print(slownik2.values())
+for element in kolekcja:
+
+from math import *
+import math as m
+from math import pow
+
+m.pow()
